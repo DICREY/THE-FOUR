9542a14292dc0e856392b740db8353fc626e13ed
DICREY <dicreyraptor@gmail.com>
2024-08-14 15:57:47 -0500

    Update proc_almac_mascotas.sql

--- src/baseDatos/proc_almac_mascotas.sql
+++ src/baseDatos/proc_almac_mascotas.sql
@@ -1,7 +1,5 @@
-
-DELIMITER //
 CREATE PROCEDURE mascotas_bd.InsertarMascota(
-    IN p_codigo INT UNSIGNED,
+    IN p_id INT UNSIGNED,
     IN p_nombre VARCHAR(100),
     IN p_especie VARCHAR(100),
     IN p_raza VARCHAR(100),
@@ -10,14 +8,52 @@ CREATE PROCEDURE mascotas_bd.InsertarMascota(
     IN p_id_propietario INT UNSIGNED
 )
 BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    SET autocommit = 0;
+
+    START TRANSACTION;
+
     INSERT INTO mascotas_bd.mascotas VALUES
-    (p_codigo,p_nombre,p_especie,p_raza,p_edad,p_peso,p_id_propietario);
+    (p_id,p_nombre,p_especie,p_raza,p_edad,p_peso,p_id_propietario);
+
+    COMMIT;
+    SET autocommit = 1;
 END //
-CREATE PROCEDURE mascotas_bd.EliminarMascota(
-    IN p_codigo INT UNSIGNED
+
+CREATE PROCEDURE mascotas_bd.ActualizarMascota(
+    IN p_id INT UNSIGNED,
+    IN p_nombre VARCHAR(100),
+    IN p_especie VARCHAR(100),
+    IN p_raza VARCHAR(100),
+    IN p_edad INT UNSIGNED, 
+    IN p_peso FLOAT,
+    IN p_id_propietario INT UNSIGNED
 ) 
 BEGIN
-    DELETE FROM mascotas_bd.mascotas WHERE codigo = p_codigo;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    SET autocommit = 0;
+
+    START TRANSACTION;
+
+    UPDATE mascotas_bd.mascotas
+    SET nombre = p_nombre,
+        especie = p_especie,
+        raza = p_raza,
+        edad = p_edad,
+        peso = p_peso,
+        id_propietario = p_id_propietario
+    WHERE id = p_id;
+    
+    COMMIT;
+    SET autocommit = 1;
 END //
-/* CALL EliminarPropietario(2) */
-DELIMITER ;
