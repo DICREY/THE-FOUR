b024d2b2c031b27a02f8f4583edb439310b68a21
JViiolett <juanavioleta1325@gmail.com>
2024-08-15 15:49:10 -0500

    V1.16

--- src/config/menu_mascota.py
+++ src/config/menu_mascota.py
@@ -14,32 +14,33 @@ def main():
 
             while True:
                 try:
-                    opcion = int ("Seleccione una opcion: ")
+                    opcion = int(input("Seleccione una opcion: "))
                     break
                 except ValueError:
                     print("Opción no válida")
                     
             if opcion == 5: 
-                print("")
+                system("cls")
+                print("Gracias por utilizar nuestra pagina")
                 break
             
+        
             elif opcion == 1:
                 system("cls")
                 print("1. Registrar Mascota")
-                
                 inser_mascota = Mascota()
-                inser_mascota.registrar_mascota
+                inser_mascota.InsertarMascota()
             
             elif opcion == 2:
                 system("cls")
                 inser_mascota = Mascota()
-                codigo_mascota = int(input("Codigo de mascota a buscar"))
-                inser_mascota.buscar_mascota(codigo_mascota)
+                id = int(input("Codigo de mascota a buscar: "))
+                inser_mascota.buscar_mascota(id)
             
             elif opcion == 3:
                 system("cls")
                 inser_mascota = Mascota()
-                codigo_mascota = int(input("Codigo de mascota a actualiza"))
+                id = int(input("Codigo de mascota a actualiza: "))
             
             elif opcion == 4:
                 system("cls")
