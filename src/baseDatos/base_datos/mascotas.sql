9f13c94a2eab4e7728e9d8b3d1f64f9622ddf08c
ITSREEN <julivelez337@gmail.com>
2024-08-30 15:31:40 -0500

    V1.60

--- src/baseDatos/base_datos/mascotas.sql
+++ src/baseDatos/base_datos/mascotas.sql
@@ -1,4 +1,4 @@
--- Active: 1716227707303@@127.0.0.1@3306@mascotas_db
+-- Active: 1724263333991@@127.0.0.1@3306@mascotas_db
 CREATE DATABASE mascotas_db;
 CREATE TABLE mascotas_db.usuarios(
     id_usuario VARCHAR(20) UNIQUE PRIMARY KEY NOT NULL,
@@ -95,3 +95,4 @@ CREATE TABLE mascotas_db.citas(
     ON UPDATE CASCADE,
     FOREIGN KEY(mascota) REFERENCES mascotas(id)
 );
+select*from servicios where id=11
